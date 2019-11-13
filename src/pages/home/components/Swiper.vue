1525232c982da3ddef1eca4aceb90956a592636d
zhy-g <1926963751@qq.com>
2019-11-13 14:21:52 +0800

    change

--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -1,27 +1,36 @@
 <template>
-    <div>
-    <swiper :options="swiperOption" >
-    <!-- slides -->
-    <swiper-slide>
-        <img class="swiper-img" src='http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/201910/fcc952d6b479db0e7e61922f3ee70f04.jpg_750x200_c5b67f70.jpg' />
-    </swiper-slide>
-    <swiper-slide>
-        <img class="swiper-img" src='http://img1.qunarzz.com/piao/fusion/1706/b9/6b91b49794f46402.jpg_750x200_d7f254e2.jpg' /></swiper-slide>
-    <swiper-slide>I'm Slide 3</swiper-slide>
-    <swiper-slide>I'm Slide 4</swiper-slide>
-   
-    <!-- Optional controls -->
-    <div class="swiper-pagination"  slot="pagination"></div>
-  </swiper>
+    <div class="wrapper">
+        <swiper :options="swiperOption" >
+        <!-- slides -->
+            <swiper-slide v-for='item of swiperList' :key='item.id'>
+                <img class="swiper-img" :src='item.imgUrl' />
+            </swiper-slide>
+        
+            <!-- Optional controls -->
+            <div class="swiper-pagination"  slot="pagination"></div>
+         </swiper>
     </div>
 </template>
 
 <script>
 export default {
     name: 'HomeSwiper',
-    data(){
+    data (){
         return {
-            swiperOption:{}
+            swiperOption:{
+                pagination:'.swiper-pagination',
+                loop:true
+            },
+            swiperList:[{
+                id: '001',
+                imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/201910/fcc952d6b479db0e7e61922f3ee70f04.jpg_750x200_c5b67f70.jpg'
+            },{
+                id: '002',
+                imgUrl: 'http://img1.qunarzz.com/piao/fusion/1706/b9/6b91b49794f46402.jpg_750x200_d7f254e2.jpg'
+            },{
+                id: '003',
+                imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/201911/2b3a9445cc37885eb13e0402baef9f5e.png_750x200_24df2b41.png'
+            }]
         }
     }
    
@@ -29,6 +38,15 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
- .swiper-img
-   width 100%
+  .wrapper >>> .swiper-pagination-bullet-active
+    background: #fff;
+  .wrapper
+    overflow hidden
+    width 100%
+    height 0
+    padding-bottom 26.66%
+    background #ccc
+    .swiper-img
+      width 100%
+    
 </style>
